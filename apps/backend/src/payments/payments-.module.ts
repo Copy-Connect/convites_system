6bf7049b6fe3ad3ac556b492a6396d52ac07b122
Felipe Paço <lipeklc26@gmail.com>
2025-09-17 17:34:44 -0300

    Ajuste no app module para metodo post no render

--- apps/backend/src/payments/payments-.module.ts
+++ apps/backend/src/payments/payments-.module.ts
@@ -3,8 +3,11 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PagSeguroGateway } from './gateway/pagseguro.gateway';
 import { PrismaService } from '../prisma/prisma.service';
+import { PaymentsController } from './payments.controller';
+
 
 @Module({
+  controllers: [PaymentsController],
   providers: [PaymentsService, PagSeguroGateway, PrismaService],
   exports: [PaymentsService],
 })
