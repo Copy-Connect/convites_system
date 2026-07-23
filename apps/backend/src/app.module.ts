f8bbf768ce9da82046b0154a22695087413763af
João Nascimento <132512837+joaonascimentobr@users.noreply.github.com>
2026-07-23 21:55:30 +0000

    - Remoção do @nestjs/serve-static e pasta public do backend - Inserindo dependencia do mercado pago e inicializanod configurações do pagamento - Configurando Cors para endereços permitidos

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,22 +1,20 @@
-// src/app.module.ts
-import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { AuthModule } from './auth/auth.module';
-import { HealthController } from 'health.controller';
-import { join } from 'path';
-import { OrdersModule } from './orders/orders.module';
-import { PaymentsModule } from './payments/payments-.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { HealthController } from "health.controller";
+import { OrdersModule } from "./orders/orders.module";
+import { PaymentsModule } from "./payments/payments.module";
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'public'),
-    }),
-    AuthModule,
-    OrdersModule,
-    PaymentsModule,
-  ],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        OrdersModule,
+        PaymentsModule,
+    ],
 
-  controllers: [HealthController],
+    controllers: [HealthController],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,22 +5,27 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule, {
+        cors: {
+            origin: ["http://localhost:5173"],
+        }
+    });
 
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+    //TODO: avaliar se a limitação abaixo faz sentido aqui no main
+    app.use(express.json({ limit: '10mb' }));
+    app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-  // raw body só no webhook
-  app.use(
-    '/payments/pagseguro/notify',
-    express.json({
-      verify: (req: any, _res, buf) => {
-        req.rawBody = buf;
-      },
-    }),
-  );
+    // raw body só no webhook
+    app.use(
+        '/payments/pagseguro/notify',
+        express.json({
+        verify: (req: any, _res, buf) => {
+            req.rawBody = buf;
+        },
+        }),
+    );
 
-  const port = parseInt(process.env.PORT ?? '3000', 10); // <<< deixe só esta linha
-  await app.listen(port, '0.0.0.0');
+    const port = parseInt(process.env.PORT ?? '3000', 10); // <<< deixe só esta linha
+    await app.listen(port, '0.0.0.0');
 }
 bootstrap();
